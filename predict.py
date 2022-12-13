06397818b4f005aaf935854d33068048e3926701
papermooon <631803439@qq.com>
2022-12-13 14:24:17 +0800

    update

--- predict.py
+++ predict.py
@@ -1,7 +1,8 @@
 import math
 import torch.nn as nn
 from hyperPara import *
-from process import en_vocab, en_tokenizer, zh_vocab
+from process import en_vocab, en_tokenizer, zh_vocab, zh_tokenizer
+from nltk.translate.bleu_score import sentence_bleu, corpus_bleu, SmoothingFunction
 
 
 # PE采用公式计算而非训练得到
@@ -76,124 +77,215 @@ class TranslateModel(nn.Module):
 def translate(src: str):
     model = torch.load(model_dir / checkpoint)
     model.eval()
-    src = torch.tensor([0] + en_vocab(en_tokenizer(src)) + [1]).unsqueeze(0).to(device)
-    tgt = torch.tensor([[0]]).to(device)
-    for i in range(max_length):
-        out = model(src, tgt)
-        predict = out[:, -1]
-        y = torch.argmax(predict, dim=1)
-        tgt = torch.concat([tgt, y.unsqueeze(0)], dim=1)
-        if y == 1:
-            break
-    tgt = ''.join(zh_vocab.lookup_tokens(tgt.squeeze().tolist())).replace("<bos>", "").replace("<eos>", "")
-    return tgt
+    with torch.no_grad():
+        src = torch.tensor([0] + en_vocab(en_tokenizer(src)) + [1]).unsqueeze(0).to(device)
+        if src.shape[1] >= max_length:
+            src = src[:, 0:max_length]
+
+        tgt = torch.tensor([[0]]).to(device)
+        for i in range(max_length):
+            out = model(src, tgt)
+            predict = out[:, -1]
+            y = torch.argmax(predict, dim=1)
+            tgt = torch.concat([tgt, y.unsqueeze(0)], dim=1)
+            if y == 1:
+                break
+        tgt = ''.join(zh_vocab.lookup_tokens(tgt.squeeze().tolist())).replace("<bos>", "").replace("<eos>", "")
+        return tgt
 
 
 # 单样本的bm search
 def translate_bs(src: str, beam_size: int):
     model = torch.load(model_dir / checkpoint)
     model.eval()
+    with torch.no_grad():
+        base = torch.tensor([0] + en_vocab(en_tokenizer(src)) + [1]).unsqueeze(0).to(device)
+        if base.shape[1] >= max_length:
+            base = base[:, 0:max_length]
+        # src:bm_size*原句长度
+        # feed:bm_size*各自翻译长度
 
-    base = torch.tensor([0] + en_vocab(en_tokenizer(src)) + [1]).unsqueeze(0).to(device)
-    # src:bm_size*原句长度
-    # feed:bm_size*各自翻译长度
+        repeat = 1
+        complete = []
+        complete_scores = []
+        working_candidates = beam_size
+        vocal = len(zh_vocab)
 
-    repeat = 1
-    complete = []
-    complete_scores = []
-    working_candidates = beam_size
-    vocal = len(zh_vocab)
+        candidates = torch.full((beam_size, 1), 0).to(device)
+        candidates_scores = torch.zeros_like(candidates)
 
-    candidates = torch.full((beam_size, 1), 0).to(device)
-    candidates_scores = torch.zeros_like(candidates)
-
-    while working_candidates != 0:
-        if repeat == 1:
-            base_feed = torch.tensor([[0]]).to(device)
-            output = model(base, base_feed)
-        else:
-            src = base.repeat(working_candidates, 1)
-            output = model(src, candidates)
-
-        predict = output[:, -1]  # W*Vocal
-        res = torch.log_softmax(predict, 1)  # W*Vocal
-
-        if repeat == 1:
-            sum_res = res
-        else:
-            sum_res = candidates_scores + res
-
-        # 展平求topK
-        sum_res = sum_res.reshape(1, -1)  # 1,W*Vocal
-        relative_index = torch.topk(k=working_candidates, input=sum_res, dim=-1)  # 1*W
-
-        next_candidates = []
-        next_scores = []
-
-        for ele in relative_index.indices.squeeze(0):
-            real_pos = torch.tensor([int(ele % vocal)], device=device)
-            pre_index = int(ele / vocal)
-            score = sum_res[0][ele]
-
-            pre_serial = candidates[pre_index]
-            next_serial = torch.cat((pre_serial, real_pos))
-
-            if real_pos == 1 or repeat == max_length:
-                working_candidates -= 1
-                complete.append(next_serial)
-                complete_scores.append(score)
+        while working_candidates != 0:
+            if repeat == 1:
+                base_feed = torch.tensor([[0]]).to(device)
+                output = model(base, base_feed)
             else:
-                next_candidates.append(next_serial)
-                next_scores.append(score)
+                src = base.repeat(working_candidates, 1)
+                output = model(src, candidates)
 
-        if working_candidates == 0:
-            break
-        candidates = torch.stack(next_candidates, 0)
-        candidates_scores = torch.stack(next_scores, 0).unsqueeze(1)
-        repeat += 1
+            predict = output[:, -1]  # W*Vocal
+            res = torch.log_softmax(predict, 1)  # W*Vocal
 
-    complete_scores = torch.stack(complete_scores, 0)
+            if repeat == 1:
+                sum_res = res
+            else:
+                sum_res = candidates_scores + res
 
-    index = 0
-    for ele in complete:
-        complete_scores[index] /= ele.shape[0]
-        index += 1
+            # 展平求topK
+            sum_res = sum_res.reshape(1, -1)  # 1,W*Vocal
+            relative_index = torch.topk(k=working_candidates, input=sum_res, dim=-1)  # 1*W
 
-    ans_index = torch.argmax(complete_scores)
-    ans = complete[ans_index]
-    tgt = ''.join(zh_vocab.lookup_tokens(ans.tolist())).replace("<bos>", "").replace("<eos>", "")
-    return tgt
+            next_candidates = []
+            next_scores = []
+
+            for ele in relative_index.indices.squeeze(0):
+                real_pos = torch.tensor([int(ele % vocal)], device=device)
+                pre_index = int(ele / vocal)
+                score = sum_res[0][ele]
+
+                pre_serial = candidates[pre_index]
+                next_serial = torch.cat((pre_serial, real_pos))
+
+                if real_pos == 1 or repeat == max_length:
+                    working_candidates -= 1
+                    complete.append(next_serial)
+                    complete_scores.append(score)
+                else:
+                    next_candidates.append(next_serial)
+                    next_scores.append(score)
+
+            if working_candidates == 0:
+                break
+            candidates = torch.stack(next_candidates, 0)
+            candidates_scores = torch.stack(next_scores, 0).unsqueeze(1)
+            repeat += 1
+
+        complete_scores = torch.stack(complete_scores, 0)
+
+        index = 0
+        for ele in complete:
+            complete_scores[index] /= ele.shape[0]
+            index += 1
+
+        ans_index = torch.argmax(complete_scores)
+        ans = complete[ans_index]
+
+        tgt = ''.join(zh_vocab.lookup_tokens(ans.tolist())).replace("<bos>", "").replace("<eos>", "")
+        return tgt
 
 
-print(translate("I love machine learning."))
-print(translate_bs("I love machine learning.", 8))
+def get_refs():
+    f = open(testing_set_zh, encoding='utf-8')
+    line = f.readline()
+    ref = []
+    while line:
+        tmp = [zh_tokenizer(line)]
+        ref.append(tmp)
+        line = f.readline()
+    f.close()
+    return ref
 
-print(translate("I love you."))
-print(translate_bs("I love you.", 8))
 
-print(translate("One reason for this is concern about the diversion of resources from other approaches."))
-print(translate_bs("One reason for this is concern about the diversion of resources from other approaches.", 8))
+def get_candys():
+    f = open(testing_set_en, encoding='utf-8')
+    line = f.readline()
+    candy = []
+    saver = open('./dataset/' + checkpoint + '_BS.txt', "w", encoding='utf-8')
 
-print(translate("In 1997,I am a pig."))
-print(translate_bs("In 1997,I am a pig.", 8))
-#
-print(translate("maybe i made a huge mistake."))
-print(translate_bs("maybe i made a huge mistake.", 8))
+    while line:
+        ans = translate_bs(line, 3)
+        print(ans)
+        saver.writelines(ans)
+        saver.write('\n')
+        ans = zh_tokenizer(ans)
+        candy.append(ans)
+        line = f.readline()
+    f.close()
+    saver.close()
+    return candy
 
-print(translate("it is a failure."))
-print(translate_bs("it is a failure.", 8))
 
-print(translate(
-    "For example, lobbying education leaders to keep girls in school longer has contributed to providing young people with the knowledge and agency to make smart decisions about when and with whom to negotiate safe sex."))
-print(translate_bs(
-    "For example, lobbying education leaders to keep girls in school longer has contributed to providing young people with the knowledge and agency to make smart decisions about when and with whom to negotiate safe sex.",
-    8))
+def get_candy_greedy():
+    f = open(testing_set_en, encoding='utf-8')
+    line = f.readline()
+    candy = []
+    saver = open('./dataset/' + checkpoint + '_G.txt', "w", encoding='utf-8')
 
-print(translate("you are a shit translation system!"))
-print(translate_bs("you are a shit translation system!", 8))
+    while line:
+        ans = translate(line)
+        print(ans)
+        saver.writelines(ans)
+        saver.write('\n')
+        ans = zh_tokenizer(ans)
+        candy.append(ans)
+        line = f.readline()
+    f.close()
+    saver.close()
+    return candy
 
-print(translate("The NCD and AIDS communities can learn from one another."))
-print(translate_bs("The NCD and AIDS communities can learn from one another.", 8))
 
-print(translate("And, in fact, albedo modification would undoubtedly make some things worse."))
-print(translate_bs("And, in fact, albedo modification would undoubtedly make some things worse.", 8))
+def debug():
+    f = open(training_set_en, encoding='utf-8')
+    line = f.readline()
+    max = 0
+
+    while line:
+        group = en_tokenizer(line)
+        line = f.readline()
+        if max <= len(group):
+            max = len(group)
+    f.close()
+    print(max)
+    return
+
+
+def candy_Test():
+    f = open(testing_set_en, encoding='utf-8')
+    line = f.readline()
+    candy = []
+    saver = open('./dataset/' + checkpoint + '.txt', "w", encoding='utf-8')
+    ct = 0
+    while line:
+        if ct == 30:
+            f.close()
+            saver.close()
+            return
+        ans = translate_bs(line, 3)
+        print(ans)
+        saver.writelines(ans)
+        saver.write('\n')
+        ans = zh_tokenizer(ans)
+        candy.append(ans)
+        line = f.readline()
+        ct += 1
+    return candy
+
+
+def pick_apples(check):
+    smooth = SmoothingFunction()
+    ref = get_refs()
+    try1 = open('./dataset/' + check + '_G.txt', encoding='utf-8')
+    try2 = open('./dataset/' + check + '_BS.txt', encoding='utf-8')
+    line1 = try1.readline()
+    line2 = try2.readline()
+    candidates1 = []
+    candidates2 = []
+    ct = 0
+    while ct != 25278:
+        tmp1 = zh_tokenizer(line1)
+        tmp2 = zh_tokenizer(line2)
+        candidates1.append(tmp1)
+        candidates2.append(tmp2)
+        line1 = try1.readline()
+        line2 = try2.readline()
+        ct += 1
+    try1.close()
+    try2.close()
+    score_G = corpus_bleu(ref, candidates1, weights=(0.25, 0.25, 0.25, 0.25), smoothing_function=smooth.method1)
+    score_BS = corpus_bleu(ref, candidates2, weights=(0.25, 0.25, 0.25, 0.25), smoothing_function=smooth.method1)
+    print(check + "_greedy:", score_G)
+    print(check + "_beam:", score_BS)
+    return
+
+
+pick_apples(checkpoint)
+pick_apples(checkpoint_next)
